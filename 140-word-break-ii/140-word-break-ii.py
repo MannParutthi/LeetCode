dba32693072a82c55b06e98ef4114e51b2b689b2
Manan Paruthi <manan.paruthi@gmail.com>
2022-01-24 07:14:11 -0500

    Time: 39 ms (38.67%), Space: 14.3 MB (66.25%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.py
+++ 140-word-break-ii/140-word-break-ii.py
@@ -1,5 +1,9 @@
 class Solution:
-    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+    
+    def wordBreakHelper(self, s, wordDict, hashMap):
+        if s in hashMap:
+            return hashMap[s]
+            
         result = []
         if s in wordDict:
             result.append(s)
@@ -7,8 +11,12 @@ class Solution:
         for i in range(len(s)):
             leftSubStr = s[0:i]
             if leftSubStr in wordDict:
-                rightSubStrList = self.wordBreak(s[i:], wordDict)
+                rightSubStrList = self.wordBreakHelper(s[i:], wordDict, hashMap)
                 for rightSubStr in rightSubStrList:
                     result.append(leftSubStr + " " + rightSubStr)
-                    
+        
+        hashMap[s] = result    
         return result
+    
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        return self.wordBreakHelper(s, wordDict, {})
