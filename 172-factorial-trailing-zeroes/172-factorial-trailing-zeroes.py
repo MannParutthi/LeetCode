0da22f2752dd77024e179e6b535886d63f940fd2
Manan Paruthi <manan.paruthi@gmail.com>
2022-01-26 07:13:51 -0500

    Time: 32 ms (81.98%), Space: 14.2 MB (45.83%) - LeetHub

--- 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.py
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.py
@@ -6,7 +6,7 @@ class Solution:
         # 10 is formed by 2 * 5 => i.e. 2^x * 5^y - in this min(x,y) will be no of 10's
         # as we know that from 1 to N, 2 comes more times (alternate no) as compared to 5 (fifth no)
         # if we just find power of 5 - it will be the answer => 5^x - x is the answer
-        # Formula => floor(n/5) + floor(n/25) + floor(n/125) + ...... + floor(n/5^x)
+        # Formula => floor(n/5) + floor(n/25) + floor(n/125) + floor(n/5^4) + ...... + floor(n/5^x)
         
         noOfTrailingZeros = 0
         divisor = 5
