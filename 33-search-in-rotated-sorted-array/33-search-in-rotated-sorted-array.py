ae6ccee99e9c970d5abd396456d9e287ab792101
Manan Paruthi <manan.paruthi@gmail.com>
2022-02-07 05:50:59 -0500

    Time: 66 ms (31.58%), Space: 14.4 MB (86.06%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -0,0 +1,26 @@
+class Solution:
+    def modifiedBinarySearch(self, nums, target, low, high):
+        while low <= high:
+            mid = low + (high-low) // 2
+            if nums[mid] == target:
+                return mid        
+            
+            if nums[low] <= nums[mid]: # Left is strictly increasing
+                if nums[low] <= target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            
+            else: # Right is strictly increasing
+                if nums[mid] < target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+            
+        return -1 # Not found
+
+    def search(self, nums: List[int], target: int) -> int:
+        if len(nums) == 0: 
+            return -1
+        
+        return self.modifiedBinarySearch(nums, target, 0, len(nums)-1)
